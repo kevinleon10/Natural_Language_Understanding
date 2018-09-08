b657af45665549768674cdc3f07303da646b077c
kevinleon10 <lskevin97@hotmail.com>
2018-09-08 16:30:47 -0600

    Pom and src

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
+                "2018-03-16",
+                "9a250a03-2813-448c-8893-339150aafd0f",
+                "lotXkpOOkXZ6"
+        );
+
+        String text = "IBM is an American multinational technology " +
+                "company headquartered in Armonk, New York, " +
+                "United States, with operations in over 170 countries.";
+
+        EntitiesOptions entitiesOptions = new EntitiesOptions.Builder()
+                .emotion(true)
+                .sentiment(true)
+                .limit(2)
+                .build();
+
+        KeywordsOptions keywordsOptions = new KeywordsOptions.Builder()
+                .emotion(true)
+                .sentiment(true)
+                .limit(2)
+                .build();
+
+        Features features = new Features.Builder()
+                .entities(entitiesOptions)
+                .keywords(keywordsOptions)
+                .build();
+
+        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                .text(text)
+                .features(features)
+                .build();
+
+        AnalysisResults response = service
+                .analyze(parameters)
+                .execute();
+        System.out.println(response);
+    }
+}
+ 
